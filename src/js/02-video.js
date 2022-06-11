9d405eadd08d12e5b35cd42cd0c45fa5c409c6fa
Vitaliy Dianov <vitalik.dianv@gmail.com>
2022-06-11 12:30:12 +0300

    fixed task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,6 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 
 const player = new Player(iframe);
-
 setCurrentTimePlayer(getCurrentTimeLS());
 
 player.on('timeupdate', throttle(saveCurrentTimeLS, 1000));
@@ -13,12 +12,16 @@ function saveCurrentTimeLS(e) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
 }
 function getCurrentTimeLS() {
-  return JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds;
+  if (JSON.parse(localStorage.getItem('videoplayer-current-time'))) {
+    return JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds;
+  }
 }
 function setCurrentTimePlayer(time) {
   player
     .setCurrentTime(time)
-    .then(function (seconds) {})
+    .then(function (seconds) {
+      console.log();
+    })
     .catch(function (error) {
       switch (error.name) {
         case 'RangeError':
